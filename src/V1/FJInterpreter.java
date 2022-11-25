8b8817e996fc12983cda7b70809f979c481b4424
geraud simon <geraud.simon99@gmail.com>
2022-11-25 14:04:25 +0100

    refactor (FJInterpreter) method eval() in sub methods
    Co-Authored-By: Adrien JALLAIS <56276737+Naedri@users.noreply.github.com>

--- src/V1/FJInterpreter.java
+++ src/V1/FJInterpreter.java
@@ -7,11 +7,9 @@ import Parser.ExpressionP.*;
 import Utils.FJUtils;
 
 import java.util.ArrayList;
-import java.util.Optional;
 import java.util.TreeSet;
 
 public class FJInterpreter implements IInterpreter {
-    //TODO conversion de eval par des surcharges
     //CT = Map de type String (pour le nom de la classe), Expression (le type Expr en haskell)
 
     /**
@@ -20,207 +18,32 @@ public class FJInterpreter implements IInterpreter {
      * Params : Class table, Expression
      * Returns : An expression after processing one reduction step
      */
-    //TODO définir type retour
-    //Expr pour surcharge ?
-    //Retourne soit une valeur soit null
-    //TODO conversion de la méthode
-    //TODO refactor de la méthode en séparant en sous fonction pour chaque type d'objet
     @Override
     public Expr evalPrime(CT dictionnary, Expr expression) {
-        //eval' ct (CreateObject c p) = -- RC-New-Arg
-        if (expression instanceof CreateObject) {
-            ArrayList<Expr> p2 = new ArrayList<>();
-
-
-            /*let p' = Data.List.map (\x -> case (eval' ct x) of Just x' -> x') p
-            in Just (CreateObject c p')*/
-            for (Expr pElem : ((CreateObject) expression).params) {
-                //eval' ct x
-                if (evalPrime(dictionnary, pElem) != null) { //TODO vérifier null ou liste vide ou autre
-                    p2.add(evalPrime(dictionnary, pElem));
-                }
-            }
-
-            ((CreateObject) expression).params = p2;
-            return expression;
+        // eval' ct (CreateObject c p) = -- RC-New-Arg
+        if (expression instanceof CreateObject createObject) {
+            return evalPrimeAsCreateObject(dictionnary, createObject);
         }
 
-        //eval' ct (FieldAccess e f)
+        // eval' ct (FieldAccess e f)
         //return the param searched in FieldAccess in the dictionnary
         if (expression instanceof FieldAccess fieldAccess) {
-
-            //if (isValue ct e) then -- R-Field
-            if (FJUtils.isValue(dictionnary, fieldAccess.ownerCLass)) {
-                //case e of      (CreateObject c p) ->
-                if (fieldAccess.ownerCLass instanceof CreateObject e) {
-
-                    //case (fields ct c) of             Just flds ->
-                    TreeSet<Field> flds = FJUtils.fields(dictionnary, e.name);
-                    if (flds.size() != 0) {
-                        //case (Data.List.findIndex (\(tp,nm) -> f == nm) flds) of
-                        int idx = 0;
-                        boolean found = false;
-
-                        for (Field fieldToCompare : flds) {
-                            if (((FieldAccess) expression).name.contentEquals(fieldToCompare.nameField)) {
-                                found = true;
-                                break;
-                            } else {
-                                idx++;
-                            }
-                        }
-
-                        //Just idx -> Just (p !! idx)
-                        if (found) {
-                            return e.params.get(idx);
-                        }
-                    } else {
-                        return null;
-                    }
-                } else {
-                    return null;
-                }
-            } else {
-                //RC-Field
-                //case (eval' ct e) of
-                //      Just e' -> Just (FieldAccess e' f)
-                //      _ -> Nothing
-                if (evalPrime(dictionnary, ((FieldAccess) expression).ownerCLass) != null) {
-                    fieldAccess.ownerCLass = evalPrime(dictionnary, ((FieldAccess) expression).ownerCLass);
-                    return fieldAccess;
-                } else {
-                    return null;
-                }
-            }
+            return evalPrimeAsFieldAccess(dictionnary, fieldAccess);
         }
 
-        //TODO eval' ct (MethodInvk e m p) =
+        // eval' ct (MethodInvk e m p) =
         //e = parent, m = name, p = params
         if (expression instanceof MethodInvk methodInvk) {
-
-            //if (isValue ct e) then -- expression existe dans le dico
-            if (FJUtils.isValue(dictionnary, methodInvk.parent)) {
-                //if (Data.List.all (isValue ct) p) then
-                boolean isAllInCT = true;
-                for (Expr expr : methodInvk.params) {
-                    if (!FJUtils.isValue(dictionnary, expr)) {
-                        isAllInCT = false;
-                        break;
-                    }
-                }
-
-                if (isAllInCT) {
-                    //case e of      (CreateObject c p) ->
-                    if (methodInvk.parent instanceof CreateObject e) {
-                        //-- R-Invk
-
-                        //case (mbody ct m c) of    Just (fpn, e')
-                        MethodBody methodBody = FJUtils.mbody(dictionnary, methodInvk.name, e.name);
-                        if(methodBody != null) {
-                            //subst (fpn ++ ["this"]) (p ++ [e]) e' //TODO possible en immutable en créeant une copie locale. Nécessaire ?
-
-                            TreeSet<String> copyNameFields = methodBody.nameFields; //(fpn ++ ["this"])
-                            copyNameFields.add("this");
-
-                            TreeSet<Expr> copyParams = ((MethodInvk) expression).params; //(p ++ [e])
-                            copyParams.add(methodInvk.parent);
-
-                            return subst(copyNameFields, copyParams, methodBody.body);
-                        } else {
-                            //no method body
-                            return null;
-                        }
-                    } else if (methodInvk.parent instanceof Cast && ((Cast) methodInvk.parent).expr instanceof Closure) { //(Cast i (Closure cp exp)) ->
-                        //TODO
-                        Cast castParent = (Cast) methodInvk.parent;
-                        Closure closureParent = (Closure) castParent.expr;
-
-                        //case (mbody ct m i) of    Just (fpn, e')
-                        MethodBody mbody = FJUtils.mbody(dictionnary, methodInvk.name, castParent.f);
-                        if(mbody != null) {
-                            // --R-Default
-                            return subst(mbody.nameFields, methodInvk.params, mbody.body);
-                        } else {
-                            // --R-Lam
-
-                            //subst (snd (unzip cp)) p exp
-                            TreeSet<String> closureParentParamsNameFields = new TreeSet<>();//snd (unzip cp)
-                            for (Field param : closureParent.params) {
-                                closureParentParamsNameFields.add(param.nameField);
-                            }
-
-                            return subst(closureParentParamsNameFields, methodInvk.params, closureParent.body);
-                        }
-                    } else {
-                        return null;
-                    }
-                } else {
-                    // RC-Invk-Arg
-                    //let p' = Data.List.map (\x -> case (eval' ct x) of Just x' -> x') p
-                    TreeSet<Expr> pPrime = new TreeSet<>();
-
-                    //param = x
-                    Expr result;
-                    for (Expr param : methodInvk.params) {
-                        result = evalPrime(dictionnary, param);
-                        if(result != null)
-                            pPrime.add(result);
-                    }
-
-                    //in Just (MethodInvk e m p')
-                    return new MethodInvk(methodInvk.parent, methodInvk.name, pPrime);
-                }
-            } else {
-                // -- RC-Invk-Recv
-                //Just e' -> Just (MethodInvk e' m p)
-                Expr ePrime = evalPrime(dictionnary, methodInvk.parent);
-
-                if(ePrime != null) {
-                    return new MethodInvk(ePrime, methodInvk.name, methodInvk.params);
-                } else {
-                    return null;
-                }
-            }
+            return evalPrimeAsMethodInvk(dictionnary, methodInvk);
         }
-
 
         // eval' ct cc@(Cast t e) =
         //le cc@ peux être lu comme :    let cc = Cast t e    in eval'(CT ct, cc)
         if (expression instanceof Cast cast) {
-            //cast.f = t, cast.expr = e
-            //if (isValue ct e)
-            if(FJUtils.isValue(dictionnary, cast.expr)) {
-                //obj@(CreateObject c' p)
-                if(cast.expr instanceof CreateObject obj) {
-                    if(FJUtils.subtyping(dictionnary, obj.name, cast.f)) {
-                        //-- R-Cast
-                        return obj;
-                    } else {
-                        return null;
-                    }
-                } else if(cast.expr instanceof Cast castParent && castParent.expr instanceof Closure) {
-                    if(FJUtils.subtyping(dictionnary, castParent.f, cast.f)) {
-                        //-- R-Cast-Lam
-                        return castParent;
-                    } else {
-                        return null;
-                    }
-                } else {
-                    //-- annotated lambda expression is a value
-                    return cast;
-                }
-            } else {
-                //case (eval' ct e) of       Just e' -> Just (Cast t e')
-                Expr ePrime = evalPrime(dictionnary, cast.expr);
-                if(ePrime != null) {
-                    return new Cast(cast.f, ePrime);
-                } else {
-                    return null;
-                }
-            }
+            return evalPrimeAsCast(dictionnary, cast);
         }
 
-        //TODO eval' ct cl@(Closure _ _) = Just cl
+        // eval' ct cl@(Closure _ _) = Just cl
         if(expression instanceof Closure cl) {
             return cl;
         } else {
@@ -231,6 +54,212 @@ public class FJInterpreter implements IInterpreter {
         //code unreachable
         //return null;
     }
+
+    /**
+     * sub method for evalPrime() in case of expression is a CreateObject
+     * @see CreateObject
+     */
+    private Expr evalPrimeAsCreateObject(CT dictionnary, CreateObject createObject) {
+        ArrayList<Expr> p2 = new ArrayList<>();
+
+        /*let p' = Data.List.map (\x -> case (eval' ct x) of Just x' -> x') p
+            in Just (CreateObject c p')*/
+        for (Expr pElem : createObject.params) {
+            //eval' ct x
+            if (evalPrime(dictionnary, pElem) != null) { //TODO vérifier null ou liste vide ou autre
+                p2.add(evalPrime(dictionnary, pElem));
+            }
+        }
+
+        createObject.params = p2;
+        return createObject;
+    }
+
+    /**
+     * sub method for evalPrime() in case of expression is a FieldAccess
+     * @see FieldAccess
+     */
+    private Expr evalPrimeAsFieldAccess(CT dictionnary, FieldAccess fieldAccess) {
+        //if (isValue ct e) then -- R-Field
+        if (FJUtils.isValue(dictionnary, fieldAccess.ownerCLass)) {
+            //case e of      (CreateObject c p) ->
+            if (fieldAccess.ownerCLass instanceof CreateObject e) {
+
+                //case (fields ct c) of             Just flds ->
+                TreeSet<Field> flds = FJUtils.fields(dictionnary, e.name);
+                if (flds.size() != 0) {
+                    //case (Data.List.findIndex (\(tp,nm) -> f == nm) flds) of
+                    int idx = 0;
+                    boolean found = false;
+
+                    for (Field fieldToCompare : flds) {
+                        if (fieldAccess.name.contentEquals(fieldToCompare.nameField)) {
+                            found = true;
+                            break;
+                        } else {
+                            idx++;
+                        }
+                    }
+
+                    //Just idx -> Just (p !! idx)
+                    if (found) {
+                        return e.params.get(idx);
+                    }
+                }
+            }
+        } else {
+            //RC-Field
+            //case (eval' ct e) of
+            //      Just e' -> Just (FieldAccess e' f)
+            //      _ -> Nothing
+            if (evalPrime(dictionnary, fieldAccess.ownerCLass) != null) {
+                fieldAccess.ownerCLass = evalPrime(dictionnary, fieldAccess.ownerCLass);
+                return fieldAccess;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * sub method for evalPrime() in case of expression is a MethodInvk
+     * @see MethodInvk
+     */
+    private Expr evalPrimeAsMethodInvk(CT dictionnary, MethodInvk methodInvk) {
+        //if (isValue ct e) then -- expression existe dans le dico
+        if (FJUtils.isValue(dictionnary, methodInvk.parent)) {
+            //if (Data.List.all (isValue ct) p) then
+            boolean isAllInCT = true;
+            for (Expr expr : methodInvk.params) {
+                if (!FJUtils.isValue(dictionnary, expr)) {
+                    isAllInCT = false;
+                    break;
+                }
+            }
+
+            if (isAllInCT) {
+                //case e of      (CreateObject c p) ->
+                if (methodInvk.parent instanceof CreateObject e) {
+                    //-- R-Invk
+
+                    //case (mbody ct m c) of    Just (fpn, e')
+                    MethodBody methodBody = FJUtils.mbody(dictionnary, methodInvk.name, e.name);
+                    if(methodBody != null) {
+                        //subst (fpn ++ ["this"]) (p ++ [e]) e' //TODO possible en immutable en créeant une copie locale. Nécessaire ?
+
+                        TreeSet<String> copyNameFields = methodBody.nameFields; //(fpn ++ ["this"])
+                        copyNameFields.add("this");
+
+                        TreeSet<Expr> copyParams = methodInvk.params; //(p ++ [e])
+                        copyParams.add(methodInvk.parent);
+
+                        return subst(copyNameFields, copyParams, methodBody.body);
+                    }
+                } else if (methodInvk.parent instanceof Cast && ((Cast) methodInvk.parent).expr instanceof Closure) { //(Cast i (Closure cp exp)) ->
+                    //TODO
+                    Cast castParent = (Cast) methodInvk.parent;
+                    Closure closureParent = (Closure) castParent.expr;
+
+                    //case (mbody ct m i) of    Just (fpn, e')
+                    MethodBody mbody = FJUtils.mbody(dictionnary, methodInvk.name, castParent.f);
+                    if(mbody != null) {
+                        // --R-Default
+                        return subst(mbody.nameFields, methodInvk.params, mbody.body);
+                    } else {
+                        // --R-Lam
+
+                        //subst (snd (unzip cp)) p exp
+                        TreeSet<String> closureParentParamsNameFields = new TreeSet<>();//snd (unzip cp)
+                        for (Field param : closureParent.params) {
+                            closureParentParamsNameFields.add(param.nameField);
+                        }
+
+                        return subst(closureParentParamsNameFields, methodInvk.params, closureParent.body);
+                    }
+                }
+            } else {
+                // RC-Invk-Arg
+                //let p' = Data.List.map (\x -> case (eval' ct x) of Just x' -> x') p
+                TreeSet<Expr> pPrime = new TreeSet<>();
+
+                //param = x
+                Expr result;
+                for (Expr param : methodInvk.params) {
+                    result = evalPrime(dictionnary, param);
+                    if(result != null)
+                        pPrime.add(result);
+                }
+
+                //in Just (MethodInvk e m p')
+                return new MethodInvk(methodInvk.parent, methodInvk.name, pPrime);
+            }
+        } else {
+            // -- RC-Invk-Recv
+            //Just e' -> Just (MethodInvk e' m p)
+            Expr ePrime = evalPrime(dictionnary, methodInvk.parent);
+
+            if(ePrime != null) {
+                return new MethodInvk(ePrime, methodInvk.name, methodInvk.params);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * sub method for evalPrime() in case of expression is a Cast
+     * @see Cast
+     */
+    private Expr evalPrimeAsCast(CT dictionnary, Cast cast) {
+        //cast.f = t, cast.expr = e
+        //if (isValue ct e)
+        if(FJUtils.isValue(dictionnary, cast.expr)) {
+            //obj@(CreateObject c' p)
+            if(cast.expr instanceof CreateObject obj) {
+                if(FJUtils.subtyping(dictionnary, obj.name, cast.f)) {
+                    //-- R-Cast
+                    return obj;
+                }
+            } else if(cast.expr instanceof Cast castParent && castParent.expr instanceof Closure) {
+                if(FJUtils.subtyping(dictionnary, castParent.f, cast.f)) {
+                    //-- R-Cast-Lam
+                    return castParent;
+                }
+            } else {
+                //-- annotated lambda expression is a value
+                return cast;
+            }
+        } else {
+            //case (eval' ct e) of       Just e' -> Just (Cast t e')
+            Expr ePrime = evalPrime(dictionnary, cast.expr);
+            if(ePrime != null) {
+                return new Cast(cast.f, ePrime);
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * -- Function: eval
+     * -- Objective: Evaluate an expression recursively.
+     * -- Params: Class table, Expression.
+     * -- Returns: A value after all the reduction steps.
+     */
+    public Expr eval(CT dictionnary, Expr expr) {
+        if(FJUtils.isValue(dictionnary, expr)) {
+            return expr;
+        } else {
+            //maybe e (eval ct) (eval' ct e)
+            if(eval(dictionnary, evalPrime(dictionnary, expr)) != null) {
+                return eval(dictionnary, evalPrime(dictionnary, expr));
+            } else {
+                return expr;
+            }
+        }
+    }
+
 
     /**
      * -- Function: subst
@@ -253,22 +282,5 @@ public class FJInterpreter implements IInterpreter {
         return null;
     }
 
-    /**
-     * -- Function: eval
-     * -- Objective: Evaluate an expression recursively.
-     * -- Params: Class table, Expression.
-     * -- Returns: A value after all the reduction steps.
-     */
-    public Expr eval(CT dictionnary, Expr expr) {
-        if(FJUtils.isValue(dictionnary, expr)) {
-            return expr;
-        } else {
-            //maybe e (eval ct) (eval' ct e)
-            if(eval(dictionnary, evalPrime(dictionnary, expr)) != null) {
-                return eval(dictionnary, evalPrime(dictionnary, expr));
-            } else {
-                return expr;
-            }
-        }
-    }
+
 }
